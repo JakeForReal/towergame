8ac227046243b288ff3a236c9305701f2a40238b
Jacob Bedard <jacob.r.bedard@gmail.com>
2026-04-04 19:32:20 -0400

    Remove B Build Mode from controls list, fix prompt blink to use alpha fade

--- scripts/StartScreen.gd
+++ scripts/StartScreen.gd
@@ -6,20 +6,15 @@ signal started()
 
 var _started := false
 var _blink_timer := 0.0
-var _prompt_visible := true
 
 @onready var _prompt_label: Label = $CanvasLayer/VBox/PromptLabel
 
-func _ready() -> void:
-	_blink_timer = 0.0
-	_prompt_visible = true
-
 func _process(delta: float) -> void:
-	_blink_timer += delta
-	if _blink_timer >= 0.6:
-		_blink_timer = 0.0
-		_prompt_visible = not _prompt_visible
-		_prompt_label.visible = _prompt_visible
+	if _started:
+		return
+	_blink_timer += delta * 2.5
+	var alpha := (sin(_blink_timer) + 1.0) * 0.5
+	_prompt_label.modulate.a = alpha
 
 func _unhandled_input(event: InputEvent) -> void:
 	if _started:
